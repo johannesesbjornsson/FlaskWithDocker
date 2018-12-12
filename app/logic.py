e0c26f0e9f74dd3c26e76a1b26c7c1b2f15a09ab
Johannes Esbjornsson <johannes.esbjornsson@gmail.com>
2018-12-12 10:12:55 +0000

    getting there

--- app/logic.py
+++ app/logic.py
@@ -1,6 +1,79 @@
 import database
 import os
 
+##################################################################
+#             Massive Function for adding food product           #
+##################################################################
+def create_textile_product(name,colour,prod_range,tags,materials):
+  if tags:
+    tags = list(set(tags))
+    tags =  [str(val) for val in tags] 
+    tags_resp = validate_tags(list(tags))
+  if materials:
+    material_resp = validate_materials(materials.keys())
+
+  insert_stmt = {'textile': [{'ID': '','Prod_range' : prod_range, 'Colour' : colour }]}
+  product_tags = format_single_column_input_with_id('Tag',list(tags))
+  if 'error' in product_tags:
+    return {'error' : {'message' : 'tags must be a list of strings', 'code' : 422 }}
+  elif product_tags:
+    insert_stmt['product_tags'] = product_tags
+
+  product_materials =format_material_input(dict(materials))
+  if 'error' in product_materials:
+    return {'error' : {'message' : 'must be dicionary of dictionaries ({"carrots": {"quantity" : 20, "unit" : "kg" }) ', 'code' : 422 }}
+  elif product_materials:
+    insert_stmt['product_materials'] =product_materials
+  try:
+    db = database.mysql() 
+    db.create_product(name,'food',insert_stmt)
+    return {'response' : {'message' : 'Product added', 'code' : 201 }}
+  except Exception as e:
+    return {'error' : {'message' : 'Internal server error', 'code' : 500 }}
+
+##################################################################
+#             Massive Function for adding textile product        #
+##################################################################
+def create_food_product(name,customer,family,tags,materials,allergens):
+  if tags:
+    tags = list(set(tags))
+    tags =  [str(val) for val in tags] 
+    tags_resp = validate_tags(list(tags))
+  if materials:
+    material_resp = validate_materials(materials.keys())
+  if allergens:
+    allergens = list(set(allergens))
+    allergens =  [str(val) for val in allergens] 
+    allergen_resp = validate_allergens(list(allergens))
+
+  insert_stmt = {'food': [{'ID': '','Customer' : customer, 'Family' : family }]}
+  product_tags = format_single_column_input_with_id('Tag',list(tags))
+  if 'error' in product_tags:
+    return {'error' : {'message' : 'tags must be a list of strings', 'code' : 422 }}
+  elif product_tags:
+    insert_stmt['product_tags'] = product_tags
+
+  product_allergens = format_single_column_input_with_id('Allergen',list(allergens))
+  if 'error' in product_allergens:
+    return {'error' : {'message' : 'allergens must be a list of strings  ', 'code' : 422 }}
+  elif product_allergens:
+    insert_stmt['product_allergens'] = product_allergens
+
+  product_materials =format_material_input(dict(materials))
+  if 'error' in product_materials:
+    return {'error' : {'message' : 'must be dicionary of dictionaries ({"carrots": {"quantity" : 20, "unit" : "kg" }) ', 'code' : 422 }}
+  elif product_materials:
+    insert_stmt['product_materials'] =product_materials
+  try:
+    db = database.mysql() 
+    db.create_product(name,'food',insert_stmt)
+    return {'response' : {'message' : 'Product added', 'code' : 201 }}
+  except Exception as e:
+    return {'error' : {'message' : 'Internal server error', 'code' : 500 }}
+
+##################################################################
+#                         Other Functions                        #
+##################################################################
 def generate_api_key(product,user):
   new_api_key = os.urandom(64).encode('base-64')
   try:
@@ -43,44 +116,12 @@ def get_product_related_tables(product_type):
   else:
     return {'error' : {'message' : 'Internal server error', 'code' : 500 }}
 
-def create_food_product(name,customer,family,tags,materials,allergens):
-  if tags:
-    tags_resp = validate_tags(list(tags))
-  if materials:
-    material_resp = validate_materials(materials.keys())
-  if allergens:
-    allergen_resp = validate_allergens(list(allergens))
-
-  insert_stmt = {'food': [{'ID': '','Customer' : customer, 'Family' : family }]}
-  product_tags = format_single_column_input_with_id('Tag',list(tags))
-  if 'error' in product_tags:
-    return {'error' : {'message' : 'tags must be a list of strings', 'code' : 422 }}
-  elif product_tags:
-    insert_stmt['product_tags'] = product_tags
-
-  product_allergens = format_single_column_input_with_id('Allergen',list(allergens))
-  if 'error' in product_allergens:
-    return {'error' : {'message' : 'allergens must be a list of strings  ', 'code' : 422 }}
-  elif product_allergens:
-    insert_stmt['product_allergens'] = product_allergens
-
-  product_materials =format_material_input(dict(materials))
-  if 'error' in product_materials:
-    return {'error' : {'message' : 'must be dicionary of dictionaries ({"carrots": {"quantity" : 20, "unit" : "kg" }) ', 'code' : 422 }}
-  elif product_materials:
-    insert_stmt['product_materials'] =product_materials
-  try:
-    db = database.mysql() 
-    db.create_product(name,'food',insert_stmt)
-    return {'response' : {'message' : 'Product added', 'code' : 201 }}
-  except Exception as e:
-    return {'error' : {'message' : str(e), 'code' : 500 }}
 
 def format_material_input(materials):
   formatted_list = []
   try:
     for material in materials:
-      insert_format = {'ID': '', 'Material' : material, 'Quantity':  materials[material]['quantity'], 'Unit':  materials[material]['unit'] }
+      insert_format = {'ID': '', 'Material' : material, 'Quantity':  round(materials[material]['quantity'],1), 'Unit':  materials[material]['unit'] }
       formatted_list.append(insert_format)
   except Exception as e:
     return ['error']
@@ -95,63 +136,53 @@ def format_single_column_input_with_id(column,values):
   except Exception as e:
     return ['error']
   return formatted_list 
-##################################################################
-##################################################################
 
 def validate_allergens(allergens):
   try:
     db = database.mysql()
     existing_allergens = db.get_allergens(allergens)
+    allergens_to_insert = remove_existing(allergens,existing_allergens)
+    if allergens_to_insert:
+      allergens_formatted = format_single_column_insert_values('Allergen',allergens_to_insert)
+      db.add_to_single_column_table('allergens',allergens_formatted)
   except Exception as e:
     return {'error' : {'message' : 'Internal server error', 'code' : 500 }}
-  allergens_to_insert = remove_existing(allergens,existing_allergens)
-  if allergens_to_insert:
-    allergens_formatted = format_single_column_insert_values('Allergen',allergens_to_insert)
-    try:
-      db.add_to_single_column_table('allergens',allergens_formatted)
-    except Exception as e:
-      return {'error' : {'message' : 'Internal server error', 'code' : 500 }}
   return 0
 
 def validate_tags(tags):
   try:
     db = database.mysql()
     existing_tags = db.get_tags(tags)
+    tags_to_insert = remove_existing(tags,existing_tags)
+    if tags_to_insert:
+      tags_formatted = format_single_column_insert_values('Tag',tags_to_insert)
+      db.add_to_single_column_table('tags',tags_formatted)
   except Exception as e:
     return {'error' : {'message' : 'Internal server error', 'code' : 500 }}
-  tags_to_insert = remove_existing(tags,existing_tags)
-  if tags_to_insert:
-    tags_formatted = format_single_column_insert_values('Tag',tags_to_insert)
-    try:
-      db.add_to_single_column_table('tags',tags_formatted)
-    except Exception as e:
-      return {'error' : {'message' : 'Internal server error', 'code' : 500 }}
   return 0
 
 def validate_materials(materials):
   try:
     db = database.mysql()
     existing_materials = db.get_materials(materials)
+    materials_to_insert = remove_existing(materials,existing_materials)
+    if materials_to_insert:
+      materials_formatted = format_single_column_insert_values('Material',materials_to_insert)
+      db.add_to_single_column_table('materials',materials_formatted)
   except Exception as e:
     return {'error' : {'message' : 'Internal server error', 'code' : 500 }}
-  materials_to_insert = remove_existing(materials,existing_materials)
-  if materials_to_insert:
-    materials_formatted = format_single_column_insert_values('Material',materials_to_insert)
-    try:
-      db.add_to_single_column_table('materials',materials_formatted)
-    except Exception as e:
-      return {'error' : {'message' : 'Internal server error', 'code' : 500 }}
   return 0
 
-def format_single_column_insert_values(column,values):
+def format_single_column_insert_values(column,values):  #  Create insert statment(s) compatible for sqlalchemy 
   insert_list =[]
   for value in values:
     insert_list.append({column: value})
   return insert_list
 
-def remove_existing(values,existing_values):
+def remove_existing(values,existing_values):   # Removing entries from values that is in existing values
+  values_lower =  [str(val).lower() for val in values]  # converting all values to lowercase strings
   for tupl in existing_values:
-    if tupl[0] in values:
-      values.remove(tupl[0])
-  return values
+    values_lower = list(filter((tupl[0]).__ne__, values_lower))  # removing all entries of strings equal to tupl
+  values_lower = list(set(values_lower))   # removing duplicate values
+  return values_lower
 
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -82,11 +82,9 @@ class TestDatabase(unittest.TestCase):
     self.assertEqual(db.create_product('name','food',prod_data), 0)
     prod = db.get_products('food',['product_tags','product_materials','product_allergens'])[0]
     self.assertEqual(type(prod['id']),int)
-    self.assertEqual(type(prod['product_tags']),list)
-    self.assertEqual(type(prod['product_materials']),list)
-    self.assertEqual(type(prod['product_allergens']),list)
-    self.assertEqual(type(prod['customer']),str )
-    self.assertEqual(type(prod['family']),str )
+    self.assertEqual(type(prod['product_tags']),list or None)
+    self.assertEqual(type(prod['product_materials']),list or None)
+    self.assertEqual(type(prod['product_allergens']),list or None)
 
   def test_add_product_2(self):
     db = self.db
@@ -114,40 +112,80 @@ class TestDatabase(unittest.TestCase):
     self.assertEqual(db.create_product('name','textile',prod_data), 0)
     prod = db.get_products('textile',['product_tags','product_materials'])[0]
     self.assertEqual(type(prod['id']),int)
-    self.assertEqual(type(prod['product_tags']),list)
-    self.assertEqual(type(prod['product_materials']),list)
+    self.assertEqual(type(prod['product_tags']),list or None)
+    self.assertEqual(type(prod['product_materials']),list or None)
     self.assertEqual(type(prod['name']),str)
-    self.assertEqual(type(prod['colour']),str)
-    self.assertEqual(type(prod['prod_range']),str)
 
 class TestLogic(unittest.TestCase):
 
   def test_remove_existing(self):
     result = logic.remove_existing(['some','some_more'],[('some_more',),('some_diff',)])
     self.assertEqual(result, ['some'])
-    result_2= logic.remove_existing(['some','some_more'],[('some_more',),('some_diff',),('some',)])
+    result_2= logic.remove_existing(['some','some_more','some'],[('some_more',),('some_diff',),('some',)])
     self.assertEqual(result_2, [])
+    result_3= logic.remove_existing(['some','Some_more'],[('some_more',),('some_diff',),('some',)])
+    self.assertEqual(result_3, [])
+    result_4= logic.remove_existing(['Something','Somet'],[('some_more',),('some_diff',),('some',)])
+    self.assertEqual(result_4,['somet','something'])
+    result_5= logic.remove_existing(['123',1,'1'],[('some_more',),('some',)])
+    self.assertEqual(result_5,['1','123'])
   
   def test_format_single_column_insert_values(self):
     column = 'Tag'
-    values = ['Test_tag','Test_tag_2']
+    values = ['aaTest_tag','aaTest_tag_2']
     result = logic.format_single_column_insert_values(column,values)
-    self.assertEqual(result, [{'Tag' : 'Test_tag'},{'Tag': 'Test_tag_2'}])
+    self.assertEqual(result, [{'Tag' : 'aaTest_tag'},{'Tag': 'aaTest_tag_2'}])
 
   def test_format_single_column_input_with_id(self):
     column = 'Tag'
-    values = ['Test_tag','Test_tag_2']
+    values = ['Testster_tag','Testster_tag_2']
     result =logic.format_single_column_input_with_id(column,values)
-    self.assertEqual(result,[{'Tag': 'Test_tag', 'ID': ''}, {'Tag': 'Test_tag_2', 'ID': ''}])
+    self.assertEqual(result,[{'Tag': 'Testster_tag', 'ID': ''}, {'Tag': 'Testster_tag_2', 'ID': ''}])
 
-  def test_add_product(self):
+  def test_add_food_product(self):
     result = logic.create_food_product('t_name','t_customer','t_family',['t_tags'],{'t_materials': {'quantity' : 20, 'unit' : 'kg' }},['t_allergen'])
+    self.assertEqual(result,{'response': {'message': 'Product added', 'code': 201}})
+
+  def test_add_textile_product(self):
+    result = logic.create_textile_product('t_name','t_colour','t_range',['t_tags'],{'t_materials': {'quantity' : 20.00, 'unit' : 'kg' }})
     self.assertEqual(result,{'response': {'message': 'Product added', 'code': 201}})
 
   def test_get_product_tables(self):
     self.assertEqual(logic.get_product_related_tables('food'), ['product_tags', 'product_materials', 'product_allergens'])
     self.assertEqual(logic.get_product_related_tables('textile'), ['product_tags', 'product_materials'])
     self.assertEqual(logic.get_product_related_tables('NOT A REAL PRODUCT'), {'error': {'message': 'Internal server error', 'code': 500}})
+
+  def test_create_food_product(self):
+    materials = {'t_materials': {'quantity' : 20, 'unit' : 'kg' }}
+    tags = ['Test_taggggg','Test_tagg_2','Test_tagg_3','Test_tagg_2']
+    allergens = ['t_allergen','t_allergen','t_allergen','t_allergen']
+    result = logic.create_food_product('test','some customer','a_family',tags,materials,allergens)
+    self.assertEqual(result,{'response': {'message': 'Product added', 'code': 201}})
+
+  def test_create_textile_product(self):
+    materials = {'t_materials': {'quantity' : 20.0001, 'unit' : 'kg' }}
+    tags = ['Test_taggggg','Test_tagg_2','Test_tagg_3','Test_tagg_2']
+    result = logic.create_textile_product('test','some_colour','some_range',tags,materials)
+    self.assertEqual(result,{'response': {'message': 'Product added', 'code': 201}})
+
+  def test_fail_create_food_product(self):
+    materials = {'quantity' : 20, 'unit' : 'kg' }
+    tags = ['Test_taggggg','Test_taggg_2','Test_tagg_3','Test_tagg_2']
+    tags_2 = ['11',11,'123']
+    allergens = ['t_allergen','t_allergen','t_allergen','t_allergen']
+    result = logic.create_food_product('test','some customer','a_family',tags,materials,allergens)
+    self.assertEqual(result,{'error': {'code': 422, 'message': 'must be dicionary of dictionaries ({"carrots": {"quantity" : 20, "unit" : "kg" }) '}} )
+    result_2 = logic.create_food_product('test','some customer','a_family',tags_2,[],allergens)
+    self.assertEqual(result_2,{'error': {'message': 'Internal server error', 'code': 500}})
+
+  def test_fail_create_textile_product(self):
+    materials = {'quantity' : 20, 'unit' : 'kg' }
+    tags = ['Test_taggggg','Test_taggg_2','Test_tagg_3','Test_tagg_2']
+    tags_2 = ['11',11,'123']
+    result = logic.create_textile_product('test','some_colour','some_range',tags,materials)
+    self.assertEqual(result,{'error': {'code': 422, 'message': 'must be dicionary of dictionaries ({"carrots": {"quantity" : 20, "unit" : "kg" }) '}} )
+    result_2 = logic.create_textile_product('test','some_colour','some_range',tags_2,[])
+    self.assertEqual(result_2,{'error': {'message': 'Internal server error', 'code': 500}})
 
 
 if __name__ == '__main__':
